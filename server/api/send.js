03d6d2f573841a8e9ed61074bc4121b6be224cbc
Skoh <101289702+SkohTV@users.noreply.github.com>
2023-05-07 18:29:20 +0200

    Update send.js

--- server/api/send.js
+++ server/api/send.js
@@ -10,44 +10,43 @@ mongoose.connect(process.env.MONGO_USER, { useNewUrlParser: true, useUnifiedTopo
 	.catch(err => console.error('Could not connect to MongoDB...', err));
 
 
-	export default async function send(req, res){
-	res.status(200).send("Hello")
-	//const [type, user, pack] = [req.body.type, JSON.parse(req.body.user), JSON.parse(req.body.pack)];
-	//let data = null;
-	//let returnValue, hashedPwd, dataDB, allow;
+export default async function send(req, res){
+	const [type, user, pack] = [req.body.type, JSON.parse(req.body.user), JSON.parse(req.body.pack)];
+	let data = null;
+	let returnValue, hashedPwd, dataDB, allow;
 
-	//try {
-	//	switch (type){
-	//		case 'CreateUser':
-	//			hashedPwd = await encryptPassword(pack.rawPwd);
-	//			dataDB = await Users.find({'name': pack.name});
+	try {
+		switch (type){
+			case 'CreateUser':
+				hashedPwd = await encryptPassword(pack.rawPwd);
+				dataDB = await Users.find({'name': pack.name});
 
-	//			if (!dataDB.length){
-	//				data = new Users({ 'name': pack.name, 'hashedPwd': hashedPwd });
-	//				await data.save();
-	//				returnValue = hashedPwd;
-	//				console.log(`Added user ${pack.name} to MongoDB database`);
-	//			} else {
-	//				returnValue = 'Denied';
-	//			}	break;
+				if (!dataDB.length){
+					data = new Users({ 'name': pack.name, 'hashedPwd': hashedPwd });
+					await data.save();
+					returnValue = hashedPwd;
+					console.log(`Added user ${pack.name} to MongoDB database`);
+				} else {
+					returnValue = 'Denied';
+				}	break;
 
-	//		case 'LoginUser':
-	//			dataDB = await Users.find({'name': pack.name});
-	//			allow = await comparePassword(pack.rawPwd, dataDB[0].hashedPwd);
-	//			returnValue = allow ? dataDB[0].hashedPwd : 'Denied'
-	//			break;
+			case 'LoginUser':
+				dataDB = await Users.find({'name': pack.name});
+				allow = await comparePassword(pack.rawPwd, dataDB[0].hashedPwd);
+				returnValue = allow ? dataDB[0].hashedPwd : 'Denied'
+				break;
 
-	//		case 'JoinMatchmaking':
-	//		case 'LeaveMatchmaking':
-	//			data = new Matchmaking({ 'username': user.name, 'gamemodes': pack.gamemodes });
-	//			await data.save();
-	//			returnValue = 'Data saved successfully';
-	//			break;
-	//	} res.status(201).send(returnValue);
-	//}
+			case 'JoinMatchmaking':
+			case 'LeaveMatchmaking':
+				data = new Matchmaking({ 'username': user.name, 'gamemodes': pack.gamemodes });
+				await data.save();
+				returnValue = 'Data saved successfully';
+				break;
+		} res.status(201).send(returnValue);
+	}
 
-	//catch (err) {
-	//	console.error(err);
-	//	res.status(500).send('Internal server error');
-	//}
+	catch (err) {
+		console.error(err);
+		res.status(500).send('Internal server error');
+	}
 };
