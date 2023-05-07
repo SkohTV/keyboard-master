2c50dbfc787dd7c4fd4e965a416d9502aa853328
Skoh <101289702+SkohTV@users.noreply.github.com>
2023-05-07 21:36:19 +0200

    Update send.js

--- server/api/send.js
+++ server/api/send.js
@@ -51,7 +51,7 @@ export default async function send(req, res){
 				const gamemodes = pack.gamemodes.split("-")
 				dataDB = await Sentences.find({"type": {$in: gamemodes}});
 				const manySentences = dataDB[Math.floor(Math.random() * dataDB.length)]
-				console.log(json.parse(manySentences), json.parse(manySentences).sentences)
+				console.log(JSON.parse(manySentences), JSON.parse(manySentences).sentences)
 				const sentence = manySentences.sentences[Math.floor(Math.random() * manySentences.length)]
 				returnValue = sentence ; break ;
 		} res.status(201).send(returnValue);
