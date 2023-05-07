7b89e23c9325a98ae682fb5de33adaae1f296199
Skoh <101289702+SkohTV@users.noreply.github.com>
2023-05-07 21:52:27 +0200

    Update sentences.js

--- server/src/models/sentences.js
+++ server/src/models/sentences.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 
 const scheme = new mongoose.Schema({
-	string: {type: String, required: true},
-	difficulty: {type: String, required: true}
+	type: {type: String, required: true},
+	difficulty: {type: [String], required: true}
 })
 
 const Sentences = mongoose.model('Sentences', scheme, 'Sentences');
